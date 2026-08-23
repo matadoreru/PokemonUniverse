c60ea8be580c51811fbea1fea13c8d83a238f341
matadoreru <martinflorithortensi@gmail.com>
2026-08-23 18:37:08 +0200

    prism

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-./apps/server/node_modules/.bin/prisma migrate deploy --schema apps/server/prisma/schema.prisma
+prisma_bin=./node_modules/.bin/prisma
+if [ ! -x "$prisma_bin" ]; then
+  prisma_bin=./apps/server/node_modules/.bin/prisma
+fi
+"$prisma_bin" migrate deploy --schema apps/server/prisma/schema.prisma
 node apps/server/dist/prisma/seed.js
 exec node apps/server/dist/src/index.js
